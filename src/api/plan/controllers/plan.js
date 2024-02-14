462e7a253486261dfc5a3b5c380203073c39d642
P3droPaulino <pedropaulinop3@gmail.com>
2024-02-14 11:35:03 -0300

    /me plans

--- src/api/plan/controllers/plan.js
+++ src/api/plan/controllers/plan.js
@@ -14,68 +14,80 @@ module.exports = createCoreController('api::plan.plan', ({ strapi }) => ({
    * @source Query Example <https://docs.strapi.io/dev-docs/backend-customization/controllers#adding-a-new-controller>
    * @method GET
    */
-async me(ctx) {
+  async me(ctx) {
     const me = ctx.state.user;
-    const query = ctx.query; //Funcionando
-    const hasFilters = query && query.filters ? true : false;
-    // console.log("[hasFilters]", query, hasFilters);
-    const filters = {
-      filters: {
+    const { filters, pagination } = ctx.query;
+
+    // Configurações de paginação
+    const page = pagination && pagination.page ? parseInt(pagination.page) : 1;
+    const pageSize = pagination && pagination.pageSize ? parseInt(pagination.pageSize) : 10;
+
+    // Inicializando os filtros com um filtro para o usuário
+    const filterConditions = {
         user: {
-          id: { $eq: me.id },
+            id: { $eq: me.id },
         },
-      },
-      populate:
-      {
-        user: true,
-        }
     };
 
-    try {
-      if (hasFilters) {
-        const oe = Object.entries(query.filters)
-        for (const a in oe) {
-          const key = oe[a][0];
-          const val = oe[a][1];
-          console.log(key, val);
-          filters.filters[key] = ["true", "false"].includes(val) ? JSON.parse(oe[a][1]) : oe[a][1]
+    // Processar filtros adicionais
+    if (filters) {
+        for (const field in filters) {
+            filterConditions[field] = filters[field];
         }
-      }
-      // console.log("FILTERS", filters, JSON.stringify(filters, null, 2));
-    } catch (e) {
-      console.log();
     }
-    const plans = await strapi.entityService.findMany('api::plan.plan', filters);
+
+    // Calcula o início com base na página e no tamanho da página
+    const start = (page - 1) * pageSize;
+
+    // Executa a consulta com paginação e filtros
+    const plans = await strapi.entityService.findMany('api::plan.plan', {
+        filters: filterConditions,
+        start,
+        limit: pageSize,
+        sort: { createdAt: 'desc' },
+        populate: '*',
+
+    });
 
     const planosProcessados = plans.map(removerCamposSensiveis);
 
+    // Calcula o total de registros para a paginação
+    const total = await strapi.entityService.count('api::withdraw.withdraw', { filters: filterConditions });
 
+
+    // Ajusta os resultados para o formato desejado
     const adjustedResults = planosProcessados.map(item => ({
-      id: item.id,
-      attributes: {
-          ...item,
-          user: {
-              data: {
-                  id: item.user.id,
-                  attributes: { ...item.user }
-              }
-          }
-      }
-  }));
+        id: item.id,
+        attributes: {
+            ...item,
+            user: {
+                data: {
+                    id: item.user.id,
+                    attributes: { ...item.user }
+                }
+            }
+        }
+    }));
 
-  // Remover as propriedades não desejadas de cada item
-  adjustedResults.forEach(item => {
-      delete item.attributes.user.id;
-      delete item.attributes.id;
-  });
-    //const statusGeral = planosProcessados.some(plano => plano.statusAtivacao);
+    // Remover as propriedades não desejadas de cada item
+    adjustedResults.forEach(item => {
+        delete item.attributes.user.id;
+        delete item.attributes.id;
+    });
 
-    //planosProcessados.push({ statusGeral: statusGeral });
-
-
-    return adjustedResults;
-
-  },
+    // Formata a resposta
+    return {
+        data: adjustedResults,
+        meta: {
+            pagination: {
+                page,
+                pageSize,
+                pageCount: Math.ceil(total / pageSize),
+                total,
+            },
+        },
+    };
+},
 
 
 
